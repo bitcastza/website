9cbd9121f396326628ca091366bc6035b8e54aa7
Kyle Robbertze <krobbertze@gmail.com>
2018-10-07 22:57:56 +0200

    enable automatic deployment

---
+++ scripts/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+URL=bitcast.co.za
+PORT=1552
+
+which ssh-agent || ( apt-get update -y && apt-get install openssh-client -y )
+eval $(ssh-agent -s)
+echo "$SSH_KEY" | tr -d '\r' | ssh-add - > /dev/null
+mkdir ~/.ssh && chmod 700 ~/.ssh
+
+hugo
+scp -r -P $PORT public/* $URL:/var/www/html/$URL
