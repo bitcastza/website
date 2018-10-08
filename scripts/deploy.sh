f4bd7c15ecb6b72f3c259e9388c1f54214c1a612
Kyle Robbertze <krobbertze@gmail.com>
2018-10-08 10:22:11 +0200

    set known ssh host id

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,6 +8,7 @@ which ssh-agent || ( apt-get update -y && apt-get install openssh-client -y )
 eval $(ssh-agent -s)
 echo "$SSH_KEY" | tr -d '\r' | ssh-add - > /dev/null
 mkdir ~/.ssh && chmod 700 ~/.ssh
+echo "$SSH_ID" >> ~/.ssh/known_hosts
 
 hugo
 scp -r -P $PORT public/* $URL:/var/www/html/$URL
