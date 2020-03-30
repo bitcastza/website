4bf78ab9f7bb1043abc786d7903c1552746af29e
Kyle Robbertze <kyle@paddatrapper.com>
2020-03-30 10:02:00 +0200

    remove CI and update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,16 +2,9 @@
 set -e
 
 CONNECTION_URI=website@sftp.bitcast.co.za
-URL=bitcast.co.za
 PORT=1552
 
-which ssh-agent || ( apt-get update -y && apt-get install openssh-client -y )
-eval $(ssh-agent -s)
-echo "$SSH_KEY" | tr -d '\r' | ssh-add - > /dev/null
-mkdir ~/.ssh && chmod 700 ~/.ssh
-echo "$SSH_ID" >> ~/.ssh/known_hosts
-
 hugo
-scp -C -o ControlMaster=auto -o ControlPersist=60s -o StrictHostKeyChecking=no -r -P $PORT public/* $CONNECTION_URI:/var/www/html/$URL
-
-ssh -C -o ControlMaster=auto -o ControlPersist=60s -o StrictHostKeyChecking=no -p $PORT $CONNECTION_URI "chown -R www-data:www-data /var/www/html/$URL"
+echo "Generated. Now run the following to upload the files:"
+echo "sftp -P $PORT $CONNECTION_URI"
+echo "mput -r public/* data/"
